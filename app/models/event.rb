e988aa404ac724c1bbc4f8d59d5de0d521c1d8cd
Mathias Bragagia <mathias.bragagia@epitech.eu>
2018-10-14 14:10:06 +0200

    Adds appointments clearing for availabilities

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,14 +10,23 @@ class Event < ApplicationRecord
 
   def self.availabilities_for_specific_day(date)
     availabilities = []
+    opening_slots = []
+    appointment_slots = []
 
-    Event.get_reccuring_openings(date, availabilities)
-    Event.get_non_reccuring_openings(date, availabilities)
+    Event.get_reccuring_openings(date, opening_slots)
+    Event.get_non_reccuring_openings(date, opening_slots)
+    Event.get_appointments(date, appointment_slots)
+
+    opening_slots.each do |opening|
+      if not appointment_slots.include?(opening)
+        availabilities << opening
+      end
+    end
 
     return availabilities
   end
 
-  def self.get_reccuring_openings(date, availabilities)
+  def self.get_reccuring_openings(date, opening_slots)
     openings = Event.where(kind: "opening", weekly_recurring: true)
     openings.each do |opening|
       op_start = opening.starts_at.to_datetime
@@ -28,32 +37,50 @@ class Event < ApplicationRecord
         days_difference = date.beginning_of_day - op_start.beginning_of_day
         if days_difference % 7 == 0
           #add opening to tab
-          Event.add_to_availabilities(op_start, op_end, availabilities)
+          Event.add_event_to_slots(op_start, op_end, opening_slots)
         end
       end
     end
 
-    return availabilities
+    return opening_slots
   end
 
-  def self.get_non_reccuring_openings(date, availabilities)
+  def self.get_non_reccuring_openings(date, opening_slots)
     openings = Event.where(kind: "opening", weekly_recurring: false, starts_at: date.beginning_of_day..date.end_of_day)
     openings.each do |opening|
       op_start = opening.starts_at.to_datetime
       op_end = opening.ends_at.to_datetime
 
-      Event.add_to_availabilities(op_start, op_end, availabilities)
+      Event.add_event_to_slots(op_start, op_end, opening_slots)
     end
 
-    return availabilities
+    return opening_slots
   end
 
-  def self.add_to_availabilities(opening_starts_at, opening_ends_at, availabilities)
-    while opening_starts_at < opening_ends_at do
-      availabilities << opening_starts_at
-      opening_starts_at += 30.minutes
+  def self.get_appointments(date, appointment_slots)
+    appointments = Event.where(kind: "appointment", starts_at: date.beginning_of_day..date.end_of_day)
+    appointments.each do |appointment|
+      at_start = appointment.starts_at.to_datetime
+      at_end = appointment.ends_at.to_datetime
+
+      Event.add_event_to_slots(at_start, at_end, appointment_slots)
     end
 
-    return availabilities
+    return appointment_slots
+  end
+
+  def self.add_event_to_slots(event_starts_at, event_ends_at, slots)
+    while event_starts_at < event_ends_at do
+      if not slots.include? Event.format_slot(event_starts_at)
+        slots << Event.format_slot(event_starts_at)
+      end
+      event_starts_at += 30.minutes
+    end
+
+    return slots
+  end
+
+  def self.format_slot(slot)
+    return slot.strftime("%H:%M")
   end
 end
